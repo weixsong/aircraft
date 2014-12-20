c800c02bf3208e0103ce0bfa473226dc91770957
weixsong <watkinsong@163.com>
2014-12-20 21:10:04 +0800

    basic layout and mouse event

--- aircraft.py
+++ aircraft.py
@@ -5,13 +5,11 @@ class Window(Tkinter.Frame):
   WINDOW_WIDTH = 800
   WINDOW_HEIGHT = 600 + 20
 
-  WINDOW_X = 400
-  WINDOW_Y = 400
+  CANVAS_WIDTH = 800
+  CANVAS_HEIGHT = 600
 
   BACKGROUND = 'white'
   TITLE = 'AirCraft watkinsong@163.com'
-
-  WINDOW = str(WINDOW_WIDTH) + 'x' + str(WINDOW_HEIGHT) + '+' + str(WINDOW_X) + '+' + str(WINDOW_Y)
 
   def __init__(self, parent):
     Tkinter.Frame.__init__(self, parent, background=Window.BACKGROUND)
@@ -19,7 +17,27 @@ class Window(Tkinter.Frame):
     self.parent.title(Window.TITLE)
     self.pack(fill=Tkinter.BOTH, expand=1)
     self.parent.resizable(width=False, height=False)
+    self.initUI()
+
+  def initUI(self):
     self.centerWindow()
+
+    self.parent.bind('<Key>', self.key)
+    self.parent.bind('<Motion>', self.motion)
+
+    self.canvas = MyCanvas(self, width=MyCanvas.CANVAS_WIDTH, height=MyCanvas.CANVAS_HEIGHT, 
+      background='black')
+    self.canvas.pack(side=Tkinter.TOP)
+    self.canvas.draw()
+
+    self.label_mouse = Tkinter.Label(self, text="mouse", background='white')
+    self.label_mouse.pack(side=Tkinter.RIGHT, expand=1)
+
+    self.label_key = Tkinter.Label(self, text="key", background='white')
+    self.label_key.pack(side=Tkinter.RIGHT, expand=1)
+
+    self.label_info = Tkinter.Label(self, text="info", background='white')
+    self.label_info.pack(side=Tkinter.RIGHT, expand=1)
 
   def centerWindow(self):
     sw = self.parent.winfo_screenwidth()
@@ -30,18 +48,26 @@ class Window(Tkinter.Frame):
     geo = '{0}x{1}+{2}+{3}'.format(str(Window.WINDOW_WIDTH), str(Window.WINDOW_HEIGHT), str(x), str(y))
     self.parent.geometry(geo)
 
-class Canvas(Tkinter.Frame):
+  def key(self, event):
+    self.parent.event_generate('<Motion>', warp=True, x=50, y=50)
+
+  def motion(self, event):
+    self.label_mouse.config(text='mouse x:{}, y:{}'.format(event.x, event.y))
+
+class MyCanvas(Tkinter.Canvas):
   CANVAS_WIDTH = 800
   CANVAS_HEIGHT = 600
-  pass
 
+  def __init__(self, root,  cnf={}, **args):
+    Tkinter.Canvas.__init__(self, root, cnf={}, **args)
 
-root = Tkinter.Tk()
-window = Window(root)
-#w = Tkinter.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, background='white')
-#w.pack()
+  def draw(self):
+    # below code is a test of canvas
+    self.create_rectangle(0, 10, 120, 80, outline="#fb0", fill="#fb0")
+    self.create_rectangle(150, 0, 240, 80, outline="#f50", fill="#f50")
+    self.create_rectangle(270, 10, 370, 80, outline="#05f", fill="#05f")
 
-#y = int(CANVAS_HEIGHT / 2)
-#w.create_line(0, y, CANVAS_WIDTH, y, fill="#476042")
-
-root.mainloop()
+if __name__ == '__main__':
+  root = Tkinter.Tk()
+  window = Window(root)
+  root.mainloop()
